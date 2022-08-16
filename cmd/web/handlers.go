c4228ba9df094f76606498d4c4d9d631f848a10c
Felipe David <felipedavid.huh@gmail.com>
2022-08-16 07:44:55 -0300

    .

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"github.com/felipedavid/not_pastebin/pkg/models"
 	"net/http"
 	"strconv"
 )
@@ -13,22 +14,31 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
+	//files := []string{
+	//	"./ui/html/home.page.tmpl",
+	//	"./ui/html/base.layout.tmpl",
+	//	"./ui/html/footer.partial.tmpl",
+	//}
 
-	ts, err := template.ParseFiles(files...)
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+
+	//err = ts.Execute(w, nil)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err)
-		return
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
 	}
 }
 
@@ -39,7 +49,17 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = fmt.Fprintf(w, "Showing snippet %v...", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "%v", s)
 	if err != nil {
 		app.serverError(w, err)
 		return
@@ -53,9 +73,16 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err := w.Write([]byte("Create a new snippet..."))
+	// Dummy data
+	title := "JP"
+	content := "This is the future authoring program..."
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"github.com/felipedavid/not_pastebin/pkg/models/mysql"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
 	// Parse command line flags
 	addr := flag.String("addr", "127.0.0.1:4000", "HTTP network address")
+	// parseTime is set so that the driver convert the database's types TIME and DATE
+	// to Go's time.Time
+	dsn := flag.String("dsn", "web:123@/not_pastebin?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
-	// Establishing the dependencies for the handlers
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a database connection pool
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
+	// Establishing dependencies for the handlers
 	app := application{
-		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
-		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	// Setting up an HTTP server that uses our app's error logger
@@ -31,6 +50,24 @@ func main() {
 	}
 
 	app.infoLog.Printf("Starting web server at %s\n", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
+}
+
+// Create a database connection pool and check if it's possible to create
+// connections
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// The 'sql.Open' don't create any connections, it just creates a pool
+	// for future use. So we need to check if we can create a connection
+	// to the database by calling Ping.
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, err
 }
