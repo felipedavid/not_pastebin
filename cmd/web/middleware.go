b078fbb26e668360d7084a02ba078ee7a8701cb7
Felipe David <felipedavid.huh@gmail.com>
2022-09-25 13:55:31 -0300

    Implementing request logging

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,42 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
-func secureHeaders(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy",
-			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
-		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("X-Frame-Options", "deny")
-		w.Header().Set("X-XSS-Protection", "0")
+func (a *app) logRequest(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		re := newResponseWriter(w)
+		next.ServeHTTP(re, r)
 
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (app *application) logRequest(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.RequestURI)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (app *application) recoverPanic(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Deferred functions are always called when the stack is being
-		// unwound following a panic
-		defer func() {
-			if err := recover(); err != nil {
-				w.Header().Set("Connection", "close")
-				app.serverError(w, fmt.Errorf("%s", err))
-			}
-		}()
-
-		next.ServeHTTP(w, r)
-	})
+		a.infoLogger.Printf("%s \"%s %s\" -> %d %s\n", r.RemoteAddr, r.Method, r.URL.Path, re.status, http.StatusText(re.status))
+	}
+	return http.HandlerFunc(fn)
 }
