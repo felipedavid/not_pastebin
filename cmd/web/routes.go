f6c6d8faa503ab5a9d9ec929d23a7c8c87ec5f03
Felipe David <felipedavid.huh@gmail.com>
2022-09-29 22:16:19 -0300

    Serving static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,9 @@ import "net/http"
 func (a *app) routes() http.Handler {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", a.home)
 	mux.HandleFunc("/snippet", a.snippet)
 
