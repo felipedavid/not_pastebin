5170a606731956c1a493e85cbd7717186da833a8
Felipe David <felipedavid.huh@gmail.com>
2022-11-05 06:48:05 -0300

    Leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,28 +2,35 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Parsing command line flags
 	addr := *flag.String("addr", "127.0.0.1:4000", "Server address")
 	flag.Parse()
 
-	// routes just match incoming requests with registered mappings and redirect
-	// them to the right handler
-	mux := http.NewServeMux() //Creating a new router
+	// Creating application's loggers
+	errLog := log.New(os.Stderr, "ERROR\t", log.Lshortfile|log.Ldate|log.Ltime)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
+	// Creating a router and setting up routes
+	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", home) // Adding a map between path "/" and the home handler
+	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view/", viewSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Printf("Starting server on %s\n", addr)
-	// Creates a web server that listens to a TCP port 4000 of every interface.
-	err := http.ListenAndServe(addr, mux)
-	fmt.Errorf(err.Error())
+	// Creating a new server and listening in 'addr'
+	server := &http.Server{
+		Addr:     addr,
+		Handler:  mux,
+		ErrorLog: errLog,
+	}
+	infoLog.Printf("Starting server on %s\n", addr)
+	err := server.ListenAndServe()
+	errLog.Fatal(err.Error())
 }
