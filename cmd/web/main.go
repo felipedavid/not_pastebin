90e903f266ab75556783e11cfac62be0822e82c4
Felipe David <felipedavid.huh@gmail.com>
2022-11-07 19:30:44 -0300

    Connecting to the database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Just a neat way to do dependency injection. If a handler or helper function
@@ -18,11 +21,19 @@ type app struct {
 func main() {
 	// Parsing command line flags
 	addr := *flag.String("addr", "127.0.0.1:4000", "Server address")
+	dsn := *flag.String("dsn", "web:pass@tcp(127.0.0.1:3306)/not_pastebin?parseTime=true", "Database service name")
 	flag.Parse()
 
 	// Creating application's loggers
 	errLog := log.New(os.Stderr, "ERROR\t", log.Lshortfile|log.Ldate|log.Ltime)
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Connecting to the database
+	db, err := openDatabase(dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// Instantiating application's dependencies
 	a := &app{
@@ -37,6 +48,22 @@ func main() {
 		ErrorLog: errLog,
 	}
 	infoLog.Printf("Starting server on %s\n", addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errLog.Fatal(err.Error())
+}
+
+// openDatabase creates a database connection pool and then checks if the database is reachable
+func openDatabase(dsn string) (*sql.DB, error) {
+	// Create a database connection pool
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check connection to the database
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
